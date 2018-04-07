bda7aa7792256b5a12879360a8369c5bd3410629
Fred Dufresne <frederick.dufresne@gmail.com>
2018-04-07 16:30:22 -0400

    Contour kind of works.

--- db.py
+++ db.py
@@ -5,9 +5,9 @@ recipes = {'Test Recipe': {'requirements': [{'item': 'Test Item 1',
                            'steps': ['Test Step 1',
                                      'Test Step 2']}}
 
-items = {'Test Item 1': {'color': [0, 255, 255],
-                         'tolerance': [16, 96, 96],
-                         'minArea': 250},
-         'Test Item 2': {'color': [40, 255, 255],
-                         'tolerance': [16, 96, 96],
-                         'minArea': 250}}
+items = {'Test Item 1': {'lower': [240, 192, 128],
+                         'upper': [15, 255, 255]},
+         'Test Item 2': {'lower': [30, 96, 64],
+                         'upper': [50, 144, 224]},
+         'Test Item 3': {'lower': [0, 0, 144],
+                         'upper': [255, 0, 255]}}
