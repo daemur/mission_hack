645d19d0fab9feab9a9421fbcd40badb2fddbbac
Fred Dufresne <frederick.dufresne@gmail.com>
2018-04-07 17:21:49 -0400

    Color fix.

--- db.py
+++ db.py
@@ -5,9 +5,10 @@ recipes = {'Test Recipe': {'requirements': [{'item': 'Test Item 1',
                            'steps': ['Test Step 1',
                                      'Test Step 2']}}
 
-items = {'Test Item 1': {'lower': [240, 192, 128],
-                         'upper': [15, 255, 255]},
-         'Test Item 2': {'lower': [30, 96, 64],
-                         'upper': [50, 144, 224]},
-         'Test Item 3': {'lower': [0, 0, 144],
-                         'upper': [255, 0, 255]}}
+items = {'Red': {'color': [0, 255, 192]},
+         'Orange': {'color': [20, 255, 192]},
+         'Yellow': {'color': [45, 255, 192]},
+         'Green': {'color': [80, 255, 192]},
+         'Blue': {'color': [140, 255, 192]},
+         'Indigo': {'color': [160, 255, 192]},
+         'Black': {'color': [0, 16, 32]}}
