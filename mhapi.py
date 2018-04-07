ef1f0542354307865ecf9ead74d9c7fd555c9749
Tyre <brandon.tyre@sap.com>
2018-04-07 11:57:06 -0400

    Adding CORS support.

--- mhapi.py
+++ mhapi.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from db import recipes
 
 app = Flask(__name__)
 api = Api(app)
-
+CORS(app, origins="*", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+    supports_credentials=True)
 
 class Recipes(Resource):
     
