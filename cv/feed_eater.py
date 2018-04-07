765ab021cd4f3ab24e67b64cdcba38c7c6754820
Fred Dufresne <frederick.dufresne@gmail.com>
2018-04-06 22:45:14 -0400

    Added first iteration of feed_eater and db.

---
+++ cv/feed_eater.py
@@ -0,0 +1,93 @@
+import cv2
+
+class StreamNotStartedException(Exception):
+    def __init__(self):
+        Exception.__init__(self, 'The stream must be started to perform this operation.')
+
+class StreamAlreadyStartedException(Exception):
+    def __init__(self):
+        Exception.__init__(self, 'The stream must be stopped to perform this operation.')
+
+class FeedEater(object):
+    def __init__(self, webcamIndex = 0):
+        '''
+        Args:
+            webcamIndex (int): The ID of the webcam to capture from.
+        '''
+        self.__stream = None
+
+    def start(self):
+        '''
+        Starts streaming the primary webcam.
+        '''
+        if self.__stream is not None:
+            raise StreamAlreadyStartedException()
+        self.__stream = cv2.VideoCapture(0)
+
+    def stop(self):
+        '''
+        Stops the webcam stream.
+        '''
+        if self.__stream is None:
+            raise StreamNotStartedException()
+        self.__stream = None
+
+    def find_items(self, items):
+        '''
+        Looks for the given list of items in the video feed.
+
+        Requires the stream to be started.
+
+        Args:
+            items (list(Item)): List of items to be searched for.
+
+        Returns:
+            list(ItemResult): List of results.
+        '''
+        results = []
+        for item in items:
+            results.append(self.find_item(item))
+        return results
+
+    def find_item(self, item):
+        '''
+        Looks for the given item in the video feed.
+
+        Requires the stream to be started.
+
+        Args:
+            item (Item): Item to be searched for.
+        '''
+        if self.__stream is None:
+            raise StreamNotStartedException()
+        return ItemResult()
+
+class ItemResult(object):
+    '''
+    Attributes:
+        item (Item): The Item that was being looked up.
+        found (bool): If the item was found.
+        position (Vector2): The position of the item, if it was found.
+    '''
+    def __init__(self, **kwargs):
+        self.item = None
+        self.found = False
+        self.position = Vector2(-1, -1)
+
+class Vector2(object):
+    '''
+    Attributes:
+        x (int): X position of the point.
+        y (int): Y position of the point.
+    '''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def value(self):
+        return [self.x, self.y]
+
+    @value.setter
+    def value(self, value):
+        self.x, self.y = value
---
+++ db/__init__.py
@@ -0,0 +1,7 @@
+recipes = {'Test Recipe': {'requirements': [{'item': 'Test Item 1',
+                                             'optional': False},
+                                            {'item': 'Test Item 2',
+                                             'optional': True}]}}
+
+items = {'Test Item 1': {},
+         'Test Item 2': {}}
