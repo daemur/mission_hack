5f2773c31863a4a4b28cb2c036081d9ca5b9cabb
Tyre <brandon.tyre@sap.com>
2018-04-07 17:54:54 -0400

    Fixing step index bug.

--- mhapi.py
+++ mhapi.py
@@ -53,7 +53,7 @@ class Step(Resource):
         global recipe
 
         recipe['steps'][id]['currentStep'] = True
-        if recipe['steps'][id - 1]:
+        if id != 0:
             recipe['steps'][id - 1]['currentStep'] = False
             recipe['steps'][id - 1]['completed'] = True
         
