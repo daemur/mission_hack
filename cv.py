ba0c57d0c6e303cb4b0a16bd1bdc8da368e3d9a1
Fred Dufresne <frederick.dufresne@gmail.com>
2018-04-07 21:38:47 -0400

    Ffmpeg trials.

--- cv.py
+++ cv.py
@@ -1,5 +1,6 @@
 from threading import Thread, Condition
 from colorsys import hsv_to_rgb
+import subprocess as sp
 import numpy as np
 import cv2
 
@@ -18,6 +19,7 @@ class FeedEater(Thread):
         self.__webcamIndex = webcamIndex
         # Thread stuff
         self.__stream = None
+        self.__ffmpeg = None
         self.__lock = Condition()
         self.__run = True
         # Recipe stuff
@@ -33,11 +35,46 @@ class FeedEater(Thread):
             self.__stream = cv2.VideoCapture(self.__webcamIndex)
             if self.__stream.isOpened():
                 print('Stream started.')
+                success, frame = self.__stream.read()
+                assert success, 'Stream started but could not capture an image.'
+                h, w, _ = frame.shape
+                self.__ffmpeg = None
+                # self.__ffmpeg = sp.Popen(['vlc',
+                #                           '--demux=rawvideo',
+                #                           '--rawvid-width={}'.format(w),
+                #                           '--rawvid-height={}'.format(h),
+                #                           '--rawvid-chroma=RV24',
+                #                           '-',
+                #                           '--sout=#transcode{vcodec=theo,vb=512,scale=1,acodec=none}:http{mux=ogg,dst=:8090/feed}',
+                #                           ':no-sout-rtp-sap',
+                #                           ':no-sout-standard-sap',
+                #                           ':ttl=1',
+                #                           ':sout-keep'],
+                #                          stdin = sp.PIPE)
+                # self.__ffmpeg = sp.Popen(['ffmpeg',
+                #                           '-f', 'rawvideo',
+                #                           '-pixel_format', 'bgr24',
+                #                           '-video_size', '{}x{}'.format(w, h),
+                #                           '-i', '-',
+                #                           '-preset', 'ultrafast',
+                #                           '-vcodec', 'h264',
+                #                           '-acodec', 'none',
+                #                           '-tune', 'zerolatency',
+                #                           '-f', 'rtp',
+                #                           'rtp://localhost:8090'],
+                #                          stdin = sp.PIPE)
+                # self.__ffmpeg = sp.Popen(['ffmpeg',
+                #                           '-f', 'rawvideo',
+                #                           '-pixel_format', 'bgr24',
+                #                           '-video_size', '{}x{}'.format(w, h),
+                #                           '-i', '-',
+                #                           '-acodec', 'libvorbis',
+                #                           '-vcodec', 'libtheora'
+                #                           '-f', 'ogg',
+                #                           'feed.ogv'],
+                #                          stdin = sp.PIPE)
                 while self.__run:
                     # Grab a frame
-                    success, frame = self.__stream.read()
-                    if not success:
-                        break
                     success, frame = self.__stream.read()
                     if not success:
                         break
@@ -71,14 +108,20 @@ class FeedEater(Thread):
                                     cv2.drawContours(frame, [contour], 0, (255, 128, 0), 4)
                         self.__print = False
                     # Forward stream to ffmpeg
+                    # self.__ffmpeg.stdin.write(frame.tostring())
                     # TODO - the real thing
                     cv2.imshow('tmp output', frame)
                     # Press escape to quit
                     if cv2.waitKey(1) == 27:
-                        break
+                       break
                 self.__stream.release()
             self.__stream = None
         finally:
+            # Kill ffmpeg
+            if self.__ffmpeg is not None:
+                self.__ffmpeg.kill()
+                self.__ffmpeg = None
+            # Kill webcam stream
             if self.__stream is not None:
                 self.__stream.release()
                 self.__stream = None
